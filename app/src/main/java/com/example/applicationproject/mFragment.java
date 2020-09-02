56d0d487fecc92bbc6391106702540edf5695fbc
Edem <atamuratov.edem@gmail.com>
2020-09-02 19:37:13 +0300

    Minor fixes.

--- app/src/main/java/com/example/applicationproject/mFragment.java
+++ app/src/main/java/com/example/applicationproject/mFragment.java
@@ -1,16 +1,12 @@
 package com.example.applicationproject;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.AudioAttributes;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -91,7 +87,7 @@ public class mFragment extends Fragment {
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setContentIntent(contentIntent);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Notifications for Android 8+
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
             channel.enableVibration(true);
             channel.setLightColor(Color.BLUE);
