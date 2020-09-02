90e4fc19153906957cd342c3a62a4432ea222062
Edem <atamuratov.edem@gmail.com>
2020-09-02 18:56:25 +0300

    Fixed notifications on Android 8+.

--- app/src/main/java/com/example/applicationproject/mFragment.java
+++ app/src/main/java/com/example/applicationproject/mFragment.java
@@ -6,12 +6,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
@@ -71,21 +74,53 @@ public class mFragment extends Fragment {
                         Notifications function
  ---------------------------------------------------------------*/
     private void callNotification(Context context, int currentPage){
+
+        String CHANNEL_ID = "ApplicationAppID", CHANNEL_NAME = "ApplicationApp";
+
         Intent startIntent = new Intent(context, MainActivity.class); // Notification intent
         startIntent.putExtra("page", page);
         startIntent.putExtra("confirm", "YES");
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, startIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context); // Notification builder
-            Notification notification = new NotificationCompat.Builder(context, "myAppChannel")
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setAutoCancel(true)
-                    .setContentTitle("You create a notification")
-                    .setContentText("Notification " + currentPage)
-                    .setPriority(NotificationCompat.PRIORITY_MAX)
-                    .setContentIntent(contentIntent)
-                    .build();
-            notificationManager.notify(1, notification);
+         // Notification builder
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context, CHANNEL_ID);
+                notification.setSmallIcon(R.mipmap.ic_launcher_round)
+                .setAutoCancel(true)
+                .setContentTitle("You created a notification")
+                .setContentText("Notification " + currentPage)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentIntent(contentIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            channel.enableVibration(true);
+            channel.setLightColor(Color.BLUE);
+            channel.enableLights(true);
+            channel.setShowBadge(true);
+            notification.setChannelId(CHANNEL_ID);
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            try {
+                notificationManager.createNotificationChannel(channel);
+            }catch (NullPointerException npe)
+            {
+                AlertDialog.Builder dialog=new AlertDialog.Builder(context);
+                dialog.setMessage("Error occurred during creating notification channel")
+                        .setTitle("Error")
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int i) {}
+                                })
+                        .create();
+                dialog.show();
+            }
+            notificationManager.notify(0, notification.build());
+        }else {
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(0, notification.build());
+        }
+
     }
     void setContext(Context context){
         appContext = context;
